4d246472fb88471bae2757e17fea5a6621eee2b4
victor6021 <victor60216021@gmail.com>
2019-09-13 09:00:52 -0700

    add boolean into my data

--- app/src/main/java/com/example/sprintandroidoopchallenge/model/AgeOfEmpiresTwo.kt
+++ app/src/main/java/com/example/sprintandroidoopchallenge/model/AgeOfEmpiresTwo.kt
@@ -5,10 +5,10 @@ data class AgeOfEmpiresTwo(val civilizations: MutableList<Civilization>,
                       val structures: MutableList<Structure>,
                       val technologies: MutableList<Technology>)
 
-data class Civilization(val id: String, val expansion: String, val armyType: String)
+data class Civilization(val id: String, val expansion: String, val armyType: String, val isFavorite: Boolean)
 
-data class Unit(val id: String, val description: String, val age: String)
+data class Unit(val id: String, val description: String, val age: String, val isFavorite: Boolean)
 
-data class Structure(val id: String, val description: String, val age: String)
+data class Structure(val id: String, val description: String, val age: String, val isFavorite: Boolean)
 
-data class Technology(val id: String,val description: String, val age: String)
+data class Technology(val id: String,val description: String, val age: String, val isFavorite: Boolean)
