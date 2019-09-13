e70c9c546ee74c3759dce0c8c28b133e1b77b489
victor6021 <victor60216021@gmail.com>
2019-09-13 08:18:50 -0700

    adding model

---
+++ app/src/main/java/com/example/sprintandroidoopchallenge/model/AgeOfEmpiresTwo.kt
@@ -0,0 +1,8 @@
+package com.example.sprintandroidoopchallenge.model
+
+data class AgeOfEmpiresTwo(val civilizations: MutableList<Civilization>,
+                      val units: MutableList<Unit>,
+                      val structures: MutableList<Structure>,
+                      val technologies: MutableList<Technology>)
+
+data class Civilization(val name: String, val id: Int)
